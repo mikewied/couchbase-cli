7be73d58a295a5a012a0a2dc02f414fc1da3631a
Mike Wiederhold <mikewied06@gmail.com>
2016-04-25 10:05:00 -0700

    WIP
    Change-Id: I8fa8a72c96a310cc5bdb851b557a2051c94f544f

--- commands.py
+++ commands.py
@@ -14,7 +14,9 @@ def parse_command():
         return False
 
     command = None
-    if sys.argv[1] == "server-list":
+    if sys.argv[1] == "bucket-create":
+        command = BucketCreate()
+    elif sys.argv[1] == "server-list":
         command = ServerList()
     else:
         return False
@@ -47,6 +49,27 @@ class Command(object):
 
     def help(self):
         pass
+
+
+class BucketCreate(Command):
+    def __init__(self):
+        super(BucketCreate, self).__init__()
+        self.parser.set_usage('couchbase-cli bucket-create [options]')
+        self.parser.add_option("--bucket", dest="name", help="The name of bucket to create")
+        self.parser.add_option("--bucket-ramsize", dest="ramsize", help="The amount of memory to allocate the bucket")
+        self.parser.add_option("--bucket-replica", dest="replica_count", help="The replica count for the bucket")
+        self.parser.add_option("--bucket-type", dest="type", help="The bucket type (memcached or couchbase)")
+        self.parser.add_option("--bucket-priority", dest="priority", help="The bucket disk io priority (low or high)")
+        self.parser.add_option("--bucket-password", dest="password", help="The bucket password")
+        self.parser.add_option("--bucket-eviction-policy", dest="eviction_policy",
+                               help="The bucket eviction policy (valueOnly or fullEviction)")
+        self.parser.add_option("--enable-flush", dest="flush", help="Enable bucket flush on this bucket (0 or 1)")
+        self.parser.add_option("--enable-index-replica", dest="flush", help="Enable replica indexes (0 or 1)")
+        self.parser.add_option("--wait", dest="wait", action="store_true",
+                               help="Wait for bucket creation to complete")
+
+    def execute(self, options, args):
+        print options
 
 
 class ServerList(Command):
